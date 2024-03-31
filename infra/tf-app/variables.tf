83348b974259ae66a3750e23244311137033f3b6
ahmadsuperman <145163782+ahmadsuperman@users.noreply.github.com>
2024-03-31 11:47:54 -0400

    complete the activity

---
+++ infra/tf-app/variables.tf
@@ -0,0 +1,15 @@
+variable "client_id" {
+  description = "The Client ID for the Azure Service Principal."
+}
+
+variable "client_secret" {
+  description = "The Client Secret for the Azure Service Principal."
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Azure Service Principal."
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for the Azure resources."
+}
